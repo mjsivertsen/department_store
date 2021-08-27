999a912200ccddb76f5be90c411b5489a4d9b20d
Michaela Sivertsen <mjsivertsen@msn.com>
2021-08-26 21:13:33 -0600

    items controller setup

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,43 @@
 class ItemsController < ApplicationController
+
+def index
+end
+
+def new
+end
+
+def create
+end
+
+def edit
+end
+
+def update
+end
+
+def destroy
+end
+
+private
+
+def item_params
+  params.require(:item).permit(:name, :price, :description)
+end
+
+def set_department
+  @department = Department.find(params[:department_id])
+end
+
+def set_item
+  @item = @department.items.find(params[:id])
+end
+
+def department_params
+  params.require(:department).permit(:name, :description)
+end
+
+
+
+
+
 end
