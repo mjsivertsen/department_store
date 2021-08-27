618133fc098eca48dc8a59b290cb3a4d4d8d4393
Michaela Sivertsen <mjsivertsen@msn.com>
2021-08-26 20:46:13 -0600

    got my second model working and seeded

--- db/migrate/20210827022827_create_items.rb
+++ db/migrate/20210827022827_create_items.rb
@@ -1,6 +1,7 @@
 class CreateItems < ActiveRecord::Migration[6.1]
   def change
     create_table :items do |t|
+      t.belongs_to :department, null: false, foreign_key: true
       t.string :name
       t.float :price
       t.string :description
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_26_220545) do
+ActiveRecord::Schema.define(version: 2021_08_27_022827) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,12 +23,14 @@ ActiveRecord::Schema.define(version: 2021_08_26_220545) do
   end
 
   create_table "items", force: :cascade do |t|
+    t.bigint "department_id", null: false
     t.string "name"
     t.float "price"
     t.string "description"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "creator"
+    t.index ["department_id"], name: "index_items_on_department_id"
   end
 
+  add_foreign_key "items", "departments"
 end
